cfcb3758ebc407d732e29ff7a660048de7f22bcc
Leah Yapp <leahyapp@gmail.com>
2024-03-04 12:25:56 -0500

    notes and creation of new js files

---
+++ scripts/permission.js
@@ -0,0 +1,8 @@
+/* JavaScript to sign in clients and logout clients.
+If the client does not have sign in information, need to register them.
+We can have a forgot password button. Remember me checkbox. 
+Need register link to a new page. When clicked to register, replace current page with new page.
+After registration, link to home page. client can choose to log in again.*/
+
+
+
---
+++ scripts/user_profile.js
@@ -0,0 +1,8 @@
+/* User can see all their information. User can change everything: 
+Name, email, phone number, address 
+These cannot change by user: wedding package, order history 
+and order history would have the amount of points they got, current points.
+Inside wedding package, they can only see information. Any changes
+would have to go through appointment with us. After discussion about
+prices, the admin would add the wedding points to the user current points.
+Order history would be on a new window.*/
